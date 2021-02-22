139a3563d4c18a2d57409928db1000f43c03b01b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-22 08:44:14 +0000

    Deploy deepqmc/deepqmc.github.io to github.com/deepqmc/deepqmc.github.io.git:master

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0.post7.dev0+34dfc04',
+    VERSION: '0.3.0.post8.dev0+8e6f186',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
