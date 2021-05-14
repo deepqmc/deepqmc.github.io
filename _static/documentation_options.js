8bcbf2a26cd80b8d299720da19c59a89ee8c94aa
Jan Hermann <dev@jan.hermann.name>
2021-05-14 09:02:41 +0200

    update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0',
+    VERSION: '0.3.0.post20.dev0+7fed46e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
